678bc5aed8ddc4ff843b79c6900f6c00604b9388
test <test@debian>
2021-11-24 12:15:59 +0100

    troisième commit

--- ajout-fichier2-Herbelin.php
+++ ajout-fichier2-Herbelin.php
@@ -1,10 +1,9 @@
 <?php
-
-echo 'Ajout fichier 2 projet Torres utilisateur Herbelin'
-?>
+echo 'ajout fichier 2 projet Torres utilisateur herbelin'
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+?>
