93b9d959f3a1742f9aa0db05274d4373dcc8ca07
test <test@debian>
2021-11-24 13:00:31 +0100

    Modification du fichier index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo 'bonjour de Lucas';
+         echo 'ceci est une erreur de Herbelin2 qui a remplacé une ligne';
         ?>
     </body>
 </html>
